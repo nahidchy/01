ec8cdc69ae446ae3b2c1608755536fe50fb36e79
nahidchy <nicnahidchowdhury@gmail.com>
2022-10-01 10:31:31 +0600

    a

--- node.js
+++ node.js
@@ -13,6 +13,9 @@ document.getElementById('button-update').addEventListener('click', function () {
     const inputField = document.getElementById('input');
     const inputText = inputField.value;
     const text = document.getElementById('text-1');
+    //  if (typeof text === 'number') {
+    //      alert('input a valid name');
+    // }
     text.innerText = inputText;
 })
 document.getElementById('enter').addEventListener('click', function () {
@@ -30,4 +33,17 @@ document.getElementById('post').addEventListener('click', function () {
     h.innerText = newComment;
     allComment.appendChild(h);
     CommentBox.value = '';
+})
+document.getElementById('input-delete').addEventListener('keyup', function (event) {
+    const text = event.target.value;
+
+    if (text === 'delete') {
+        document.getElementById('delete-btn').removeAttribute('disabled')
+    }
+    else {
+        document.getElementById('delete-btn').setAttribute('disabled',true)
+    }
+})
+document.getElementById('delete-btn').addEventListener('click', function () {
+    document.getElementById('text-2').style.display = 'none';
 })
